464c9ef92381a3f6e3a3dbf67c6f44989cecfab1
Jan Tusil <jenda.tusil@gmail.com>
2021-02-07 16:51:58 +0100

    nix

---
+++ coq/shell.nix
@@ -0,0 +1,20 @@
+let
+  sources = import ./nix/sources.nix;
+  pinned = import sources."nixpkgs" { config = {}; overlays = []; };
+in
+
+{ pkgs ? pinned }:
+
+let
+  inherit (pkgs) mkShell;
+
+  /*coq-matching-logic = import sources."coq8.13-matching-logic" { inherit pkgs; };*/
+  coq-matching-logic = import sources."AML-Formalization" { };
+
+
+  self = mkShell {
+    buildInputs = [coq-matching-logic];
+  };
+in
+  self
+
